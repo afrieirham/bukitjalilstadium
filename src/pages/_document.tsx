be08b18c9c9129d92f463432a3516f92c3b87174
Afrie Irham <afrieirham.work@gmail.com>
2023-11-26 03:27:40 +0800

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token="52a61c07-85f9-440d-b47b-e9d0764e3c43"
+          async
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
