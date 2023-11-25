2e9a49566115461350e5d9ab21bcdaf642a05243
Afrie Irham <afrieirham.work@gmail.com>
2023-11-26 02:51:33 +0800

    add link to afrieirham.com

--- src/components/BuiltBy.tsx
+++ src/components/BuiltBy.tsx
@@ -3,7 +3,11 @@ import React from "react";
 function BuiltBy() {
   return (
     <div className="fixed z-50 bottom-0 right-0">
-      <a className="bg-white text-black p-2 rounded-t text-xs mx-4">
+      <a
+        href="https://afrieirham.com"
+        target="_blank"
+        className="bg-white text-black p-2 rounded-t text-xs mx-4"
+      >
         Built by @afrieirham
       </a>
     </div>
