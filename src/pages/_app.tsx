ecdbf8b38a05a48e8fd787a661bdb52565da6990
farhan-helmy <fhroslan@seekasia.com>
2023-12-06 22:59:42 +0800

    feat: add shadcn, dark mode toggle and command menu for seat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "react-photo-view/dist/react-photo-view.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
