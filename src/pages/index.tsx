699d07e719ecb8cb148d8c1abb9a84099208ed84
mohamedariff <54528430+mohamedariff@users.noreply.github.com>
2024-02-10 22:57:52 +0800

    improve seo (#7)
    * improve seo
    
    * add meaningful alt text
    
    * optimize image
    
    * misc
    
    * fix: use twitter creator and fix username typo
    
    * fix: update photo alt text
    
    ---------
    
    Co-authored-by: Mohamed Ariff <mohamed-ariff_abdul-rahim@astro.com.my>
    Co-authored-by: Afrie Irham <afrieirham.work@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,22 +7,22 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 export default function Home() {
   return (
-      <main>
-        <SEOHead
-          title="Stadium Bukit Jalil Seating View – BukitJalilStadium.com"
-          description="Field view from seats in each sections inside of the Stadium Nasional Bukit Jalil"
-          path="/"
-          ogPath="/og.png"
-        />
-        <NavBar />
-        <div className="flex flex-col items-center space-y-8 px-4 dark:bg-[#111111]">
-          <h1 className="mt-8 text-lg font-bold text-center md:text-2xl">
-            Stadium Nasional Bukit Jalil Seating View Plan
-          </h1>
-          <SectorNav />
-        </div>
-        <SeatNav />
-        <BuiltBy />
-      </main>
+    <main>
+      <SEOHead
+        title="Stadium Bukit Jalil Seating View | BukitJalilStadium.com"
+        description="Field view from seats in each sections inside of the Stadium Nasional Bukit Jalil"
+        path="/"
+        ogPath="/og.png"
+      />
+      <NavBar />
+      <div className="flex flex-col items-center space-y-8 px-4">
+        <h1 className="mt-8 text-lg font-bold text-center md:text-2xl">
+          Stadium Nasional Bukit Jalil Seating View Plan
+        </h1>
+        <SectorNav />
+      </div>
+      <SeatNav />
+      <BuiltBy />
+    </main>
   );
 }
