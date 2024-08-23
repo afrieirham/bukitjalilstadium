c723724843304e22f26bb92bd7b258c5da4bf9ad
Afrie Irham <afrieirham.work@gmail.com>
2024-08-23 18:45:49 +0800

    add donation bar

---
+++ src/components/Donation.tsx
@@ -0,0 +1,9 @@
+export default function Donation() {
+  return (
+    <div className="flex items-center justify-center py-2.5 sm:px-3.5 bg-white text-black text-xs">
+      <a target="_blank" href="https://donate.stripe.com/00g3dd6MA8yM0j6fZ2">
+        Enjoy using the website? Buy me a coffee ☕️
+      </a>
+    </div>
+  );
+}
