da445bf355f167b0634aa33cc70c0af6d6b3f07f
Afrie Irham <afrieirham.work@gmail.com>
2024-09-22 03:25:41 +0800

    add umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,11 @@ export default function Document() {
           data-token={process.env.NEXT_PUBLIC_BEAM_ANALYTICS_DATA_TOKEN}
           async
         />
+        <script
+          defer
+          src="https://analytics.afrieirham.com/script.js"
+          data-website-id="ccdf97db-6c20-4e02-8ab5-4cb945f6a4bb"
+        />
         <meta name="google-adsense-account" content="ca-pub-5654598090665312" />
         <script
           async
