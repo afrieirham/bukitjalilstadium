63da6e5b9808ee1a3ee8e2d044101ad455be97bd
Afrie Irham <afrieirham.work@gmail.com>
2026-05-21 07:55:12 +0800

    remove donation bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,17 @@
 import Link from "next/link";
-import Donation from "./Donation";
 
 function NavBar() {
   return (
-    <>
-      <Donation />
-      <nav className="flex justify-center sm:justify-between bg-[#111111] border-b-[#282828] border-b-2 p-6">
-        <Link href="/">BukitJalilStadium.com</Link>
-        <div className="flex items-center gap-2">
-          <Link href="/contribute" className="hover:underline">
-            Contribute
-          </Link>
-          <p>/</p>
-          <p className="hidden sm:block">Stadium Bukit Jalil Seating View</p>
-        </div>
-      </nav>
-    </>
+    <nav className="flex justify-center sm:justify-between bg-[#111111] border-b-[#282828] border-b-2 p-6">
+      <Link href="/">BukitJalilStadium.com</Link>
+      <div className="flex items-center gap-2">
+        <Link href="/contribute" className="hover:underline">
+          Contribute
+        </Link>
+        <p>/</p>
+        <p className="hidden sm:block">Stadium Bukit Jalil Seating View</p>
+      </div>
+    </nav>
   );
 }
 
