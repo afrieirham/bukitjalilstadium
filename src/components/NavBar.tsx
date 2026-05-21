a37aa9b39b66a60eeded151d12f9a24d808c51bd
Afrie Irham <afrieirham.work@gmail.com>
2026-05-21 20:01:34 +0800

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,14 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <nav className="flex justify-center sm:justify-between bg-[#111111] border-b-[#282828] border-b-2 p-6">
-      <Link href="/">BukitJalilStadium.com</Link>
+    <nav className="flex justify-between bg-[#111111] border-b-[#282828] border-b-2 p-6">
+      <Link href="/" className="flex items-center gap-2">
+        {/** biome-ignore lint/performance/noImgElement: <intended> */}
+        <img alt="logo" src="/logo.png" className="h-12 w-12" />
+        <span className="hidden md:block">
+          Stadium Bukit Jalil Seating View
+        </span>
+      </Link>
       <div className="flex items-center gap-2">
         <Link href="/contribute" className="hover:underline">
           Contribute
@@ -12,8 +18,6 @@ function NavBar() {
         <Link href="/contributors" className="hover:underline">
           Contributors
         </Link>
-        <p>/</p>
-        <p className="hidden sm:block">Stadium Bukit Jalil Seating View</p>
       </div>
     </nav>
   );
