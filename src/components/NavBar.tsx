737e1db0b507dd71c3d21ce67621896ae25e49f4
Afrie Irham <afrieirham.work@gmail.com>
2026-07-04 17:00:41 +0800

    update homestay ad

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,34 +2,39 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <nav className="flex justify-between bg-[#111111] border-b-[#282828] border-b-2 p-6">
-      <Link href="/" className="flex items-center gap-2">
-        {/** biome-ignore lint/performance/noImgElement: <intended> */}
-        <img alt="logo" src="/logo.png" className="h-12 w-12" />
-        <span className="hidden md:block">
-          Stadium Bukit Jalil Seating View
-        </span>
-      </Link>
-      <div className="flex items-center gap-2">
-        <div className="flex items-center justify-center gap-2">
+    <>
+      <nav className="flex justify-between bg-[#111111] border-b-[#282828] border-b-2 p-6">
+        <Link href="/" className="flex items-center gap-2">
+          {/** biome-ignore lint/performance/noImgElement: <intended> */}
+          <img alt="logo" src="/logo.png" className="h-12 w-12" />
+          <span className="hidden md:block">
+            Stadium Bukit Jalil Seating View
+          </span>
+        </Link>
+        <div className="flex items-center gap-2">
+          <Link href="/contribute" className="hover:underline">
+            Contribute
+          </Link>
+          <p>/</p>
+          <Link href="/contributors" className="hover:underline">
+            Contributors
+          </Link>
+        </div>
+      </nav>
+
+      <div className="flex items-center justify-center gap-2 mt-8">
+        <Link
+          href="https://tally.so/r/obQDqP"
+          className="flex items-center gap-2 bg-white px-2 hover:bg-white/90 py-1 rounded"
+        >
           <div className="relative h-2 w-2">
             <div className="absolute top-0 bottom-0 h-2 w-2 rounded-full bg-emerald-500" />
             <div className="absolute top-0 bottom-0 h-2 w-2 rounded-full bg-emerald-500 animate-ping" />
           </div>
-          <Link href="https://tally.so/r/obQDqP" className="hover:underline">
-            Looking for homestay?
-          </Link>
-        </div>
-        <p>/</p>
-        <Link href="/contribute" className="hover:underline">
-          Contribute
-        </Link>
-        <p>/</p>
-        <Link href="/contributors" className="hover:underline">
-          Contributors
+          <span className="text-xs text-black">Looking for homestay?</span>
         </Link>
       </div>
-    </nav>
+    </>
   );
 }
 
