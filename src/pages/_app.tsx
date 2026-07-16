ee71b56a7fdea31f4463e7812e0d63c76310dc2d
Afrie Irham <32460534+afrieirham@users.noreply.github.com>
2026-07-16 13:39:52 +0800

    Change PostHog initialization parameters
    Updated PostHog initialization with hardcoded token and API host.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import "react-photo-view/dist/react-photo-view.css";
 import posthog from "posthog-js";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  posthog.init("phc_teM97m62HSztsrdRBajuuVthJ3M5XWifpkCYoydHhPaP", {
+    api_host: "https://eu.i.posthog.com",
     ui_host: "https://eu.posthog.com",
     defaults: "2026-01-30",
     capture_exceptions: true,
