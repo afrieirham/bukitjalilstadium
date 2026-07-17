29d2aa8d6496555c1cc172905cc9f54d6293d955
Afrie Irham <afrie@chip-in.asia>
2026-07-17 19:07:42 +0800

    fix eslint

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,6 @@
-import { Button } from "~/components/core/button";
 import SeatPlanPicker from "~/components/widget/seat-plan-picker";
 
-import type { Route } from "./+types/home";
-
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
