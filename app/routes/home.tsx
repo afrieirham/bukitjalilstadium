493dac91f0faaa5b944b758e8f989689839d29ed
Afrie Irham <afrie@chip-in.asia>
2026-07-17 10:36:12 +0800

    setup shadcn, housekeeping

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/core/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="py-8">
+      <Button>hello world</Button>
+    </div>
+  );
 }
